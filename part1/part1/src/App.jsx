569e7d6e75e27c419430d75b15e6ec798528cb21
neandermenezes <neander2801@gmail.com>
2023-12-04 12:34:21 -0300

    feat: exercise 1.3

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -3,19 +3,33 @@ import Header from "./components/Header"
 import Total from "./components/Total"
 
 const App = () => {
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       {<Header course={course} />}
-      {<Content subjects={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />}
-      <p>Number of exercises {<Total totalExercises={exercises1 + exercises2 + exercises3} />}</p>
+      {<Content subjects={[part1.name, part2.name, part3.name]} exercises={[part1.exercises, part2.exercises, part3.exercises]} />}
+      <p>Number of exercises {<Total totalExercises={part1.exercises + part2.exercises + part3.exercises} />}</p>
     </div>
   )
 }
